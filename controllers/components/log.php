958919c12daa1a7346b0c88fe950a18a607112c0
Joe Beeson <jbeeson@gmail.com>
2010-01-17 15:36:07 -0700

    LogComponent::$levels now uses the constants themselves since they can change

--- controllers/components/log.php
+++ controllers/components/log.php
@@ -13,27 +13,26 @@
 		
 		/**
 		 * Helps us translate error integers back into their respective
-		 * human readable levels.
+		 * human readable labels. We use the constant because their values
+		 * can change between certain PHP versions.
 		 * @var array
 		 * @static
 		 */
 		public static $levels = array(
-			1     => 'E_ERROR',
-			2     => 'E_WARNING',
-			4     => 'E_PARSE',
-			8     => 'E_NOTICE',
-			16    => 'E_CORE_ERROR',
-			32    => 'E_CORE_WARNING',
-			64    => 'E_COMPILE_ERROR',
-			128   => 'E_COMPILE_WARNING',
-			256   => 'E_USER_ERROR',
-			512   => 'E_USER_WARNING',
-			1024  => 'E_USER_NOTICE',
-			2048  => 'E_STRICT',
-			4096  => 'E_RECOVERABLE_ERROR',
-			8192  => 'E_DEPRECATED',
-			16384 => 'E_USER_DEPRECATED',
-			30719 => 'E_ALL'
+			E_ERROR 			=> 'E_ERROR',
+			E_WARNING			=> 'E_WARNING',
+			E_PARSE				=> 'E_PARSE',
+			E_NOTICE			=> 'E_NOTICE',
+			E_CORE_ERROR		=> 'E_CORE_ERROR',
+			E_CORE_WARNING    	=> 'E_CORE_WARNING',
+			E_COMPILE_ERROR    	=> 'E_COMPILE_ERROR',
+			E_COMPILE_WARNING   => 'E_COMPILE_WARNING',
+			E_USER_ERROR   		=> 'E_USER_ERROR',
+			E_USER_WARNING   	=> 'E_USER_WARNING',
+			E_USER_NOTICE  		=> 'E_USER_NOTICE',
+			E_STRICT  			=> 'E_STRICT',
+			E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+			E_DEPRECATED  		=> 'E_DEPRECATED',
 		);
 		
 		/**
